4b790a1a15b6e286c9a4be6c9ecbce87c75840e2
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-08 18:57:04 -0300

    sa

--- login.php
+++ login.php
@@ -91,7 +91,7 @@
                                 <label
                                     class="list-group-item d-flex align-items-center mb-3 font-size-14 text-white fw-500"><input
                                         class="form-check-input m-0 me-2" type="checkbox">Remember Me</label>
-                                <div class="full-button">
+                                <div class="">
                                     <div class="iq-button active">
                                         <a href="#" class="btn  text-uppercase position-relative">
                                             <span class="button-text">log in</span>
