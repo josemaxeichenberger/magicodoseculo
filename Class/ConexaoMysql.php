6ec4116e37bb245840aab8a798973970c652d791
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-09 12:41:37 -0300

    s

--- Class/ConexaoMysql.php
+++ Class/ConexaoMysql.php
@@ -6,22 +6,18 @@ require_once 'Config.php';
 class ConexaoMysql {
 
   protected $pdo;
- public function __construct() {
-      
+
+  public function __construct() {
     try {
-      $this->pdo = new PDO('mysql:host=' . DB_HOUST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+      // Configuração de conexão com o charset utf8
+      $dsn = 'mysql:host=' . DB_HOUST . ';dbname=' . DB_NAME . ';charset=utf8';
+      $this->pdo = new PDO($dsn, DB_USER, DB_PASS);
       $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $_SESSION['conect'] =  "Exito";      
+      $_SESSION['conect'] = "Exito";      
     } catch (PDOException $e) {
-       $_SESSION['conect'] =  "Falha";
-     
-      
+      $_SESSION['conect'] = "Falha";
     }
   }
-  
-  
-
-
 }
 
 ?>
