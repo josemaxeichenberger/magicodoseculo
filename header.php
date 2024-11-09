709564f9fb3eab48c4ab7a3ce8b19d0da5245445
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-09 10:27:24 -0300

    sa

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
                      
                           <div class="logo-hotstar">
                               <a class="navbar-brand text-primary" href="./index.html">
-                                  <img class="img-fluid logo" src="./assets/images/logo-hotstar.webp" loading="lazy" alt="streamit" />
+                                  <img class="img-fluid logo" src="assets/identidade/001.png" width="70" loading="lazy" alt="streamit" />
                               </a>
                           </div>
                           
