44a1fd9e83ae2feee50fc1524e5c2404c93f9b96
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-09 11:43:56 -0300

    s

--- Class/Config.php
+++ Class/Config.php
@@ -1,5 +1,5 @@
 <?php
- define('DB_HOUST','216.59.16.157');
+ define('DB_HOUST','localhost');
  define('DB_USER','magicodoseculo_base');
  define('DB_PASS','dire@0300');
  define('DB_NAME','magicodoseculo_base');
