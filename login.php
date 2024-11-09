7a67b6105cd9f153d0464724c5d79f852675667a
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-08 22:04:43 -0300

    sa

--- login.php
+++ login.php
@@ -76,6 +76,7 @@
                                 </div>
                             </div>
                             <form action="post">
+                                <h2>Conecte <br> <span class="fw-bold">sua conta</span></h2>
                                 <div class="mb-3">
                                     <label class="text-white fw-500 mb-2">Username or Email Address</label>
                                     <input type="text" class="form-control rounded-0" required="">
