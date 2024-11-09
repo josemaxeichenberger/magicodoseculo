fa287d161011a0720295641d3a321a660019738a
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-09 13:16:14 -0300

    sa

--- index.php
+++ index.php
@@ -187,8 +187,11 @@ spl_autoload_register("my_autoload"); // Carrega as classes
       <div class="container-fluid">
         <div class="overflow-hidden">
           <div class="d-flex align-items-center justify-content-between px-md-3 px-1 mb-4">
-            <h5 class="main-title text-capitalize mb-0">Histórico de Aulas</h5> 
-            <span>Acesse as últimas aulas que assistiu</span>
+            <h5 class="main-title text-capitalize mb-0">Histórico de Aulas
+              <br>
+              <span class="fs-6 text-bg-light">Acesse as últimas aulas que assistiu</span>
+            </h5> 
+            
             <a href="view-all-movie.html" class="text-primary iq-view-all text-decoration-none flex-none">View All</a>
 
             
