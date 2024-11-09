abb6ccf82bda3cf7dec378c40da1e7d0b1a584fa
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-08 23:10:23 -0300

    sa

--- footer.php
+++ footer.php
@@ -4,40 +4,28 @@
         <div class="footer-bottom ">
             <div class="row align-items-center">
                 <div class="logo-hotstar">
-                    <a class="navbar-brand text-primary" href="./index.html">
+                    <a class="navbar-brand text-primary" href="/">
                         <img class="img-fluid logo" src="./assets/identidade/RodapeLogo.png" loading="lazy" alt="streamit" />
                     </a>
                 </div>
                 <div class="col-md-6">
                     <ul class="menu list-inline p-0 d-flex flex-wrap align-items-center">
                         <li class="menu-item">
-                            <a href="#"> Terms Of Use </a>
+                            <a href="#"> Termos de uso </a>
                         </li>
                         <li id="menu-item-7316" class="menu-item">
-                            <a href="./privacy-policy.html"> Privacy-Policy </a>
+                            <a href="./privacy-policy.html"> Política de Privacidade </a>
                         </li>
                         <li class="menu-item">
-                            <a href="./faq.html"> FAQ </a>
+                            <a href="./faq.html"> Perguntas frequentes </a>
                         </li>
                         <li class="menu-item">
-                            <a href="./playlist.html"> Watch List </a>
+                            <a href="./playlist.html"> Lista de observação </a>
                         </li>
                     </ul>
-                    <p class="font-size-14">© <span class="currentYear"></span> <span class="text-primary">STREAMIT</span>. All Rights Reserved. All videos and shows on this platform are trademarks of, and all related images and content are the property of, Streamit Inc. Duplication and copy of this is strictly prohibited. All rights reserved. </p>
+                    <p class="font-size-14">© <span class="currentYear"></span> <span class="text-primary">ROLETA MÁGICA</span>. Todos os direitos reservados. Todos os vídeos informativos neste aplicativo são marcas registradas e todas as imagens e conteúdos relacionados são propriedade do Mágico do Século. <br> <br>A duplicação e plágio disto são estritamente proibidos. </p>
                 </div>
-                <div class="col-md-3"></div>
-                <div class="col-md-3">
-                    <h6 class="font-size-14 pb-1">Download Streamit Apps </h6>
-                    <div class="d-flex align-items-center">
-                        <a class="app-image" href="#">
-                            <img src="./assets/images/footer/google-play.webp" loading="lazy" alt="play-store" />
-                        </a>
-                        <br />
-                        <a class="ms-3 app-image" href="#">
-                            <img src="./assets/images/footer/apple.webp" loading="lazy" alt="app-store" />
-                        </a>
-                    </div>
-                </div>
+               
             </div>
         </div>
     </div>
