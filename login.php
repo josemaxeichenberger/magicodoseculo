0cd4280e2ea90bbc2e2d75b0c00626b7abc02b60
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-08 21:54:15 -0300

    sa

--- login.php
+++ login.php
@@ -63,7 +63,7 @@
 
 
         <div class="">
-            <img src="assets/identidade/HomeBanner.png" class="img-fluid d-block" alt="">
+            <img src="assets/identidade/HomeBanner.png" class="img-fluid d-block mx-auto" alt="">
             <div class="container">
                 <div class="row justify-content-center align-items-center height-self-center vh-100">
                     <div class="col-lg-5 col-md-12 align-self-center">
