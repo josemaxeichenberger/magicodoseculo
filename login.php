7f45bddbf85bcdc580ec9949d17237d02a4c1ab6
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-08 22:26:47 -0300

    sa

--- login.php
+++ login.php
@@ -55,10 +55,10 @@
     </div>
     <!-- loader END --> <!-- loader END -->
     <main class="main-content">
-        <nav class="navbar bg-body-tertiary">
+        <nav class="navbar bg-body-tertiary  bg-primary">
             <div class="container">
                 <a class="navbar-brand" href="#">
-                    <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+                    <img src="assets/identidade/001.png" alt="Bootstrap" width="30" height="24">
                 </a>
             </div>
         </nav>
