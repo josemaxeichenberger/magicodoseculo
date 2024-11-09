3749a4c6391df4e3ca956b7c633166a5a5fa799a
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-09 10:25:43 -0300

    sa

--- header.php
+++ header.php
@@ -42,38 +42,14 @@
                           <!-- container-fluid.// -->
                       </nav>
                       <div class="right-panel">
-                          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                              aria-label="Toggle navigation">
-                              
-                          </button>
+                       
                           <ul class="navbar-nav align-items-center ms-auto mb-2 mb-xl-0">
                               
                               <li class="nav-item dropdown" id="itemdropdown1">
                                   <a class="nav-link d-flex align-items-center" href="#" id="navbarDropdown" role="button"
                                       data-bs-toggle="dropdown" aria-expanded="false">
-                                      <!-- <div class="btn-icon rounded-pill user-icons">
-                                          <span class="btn-inner">
-                                              <svg class="icon-18" width="18" viewBox="0 0 24 24" fill="none"
-                                                  xmlns="http://www.w3.org/2000/svg">
-                                                  <path fill-rule="evenodd" clip-rule="evenodd"
-                                                      d="M9.87651 15.2063C6.03251 15.2063 2.74951 15.7873 2.74951 18.1153C2.74951 20.4433 6.01251 21.0453 9.87651 21.0453C13.7215 21.0453 17.0035 20.4633 17.0035 18.1363C17.0035 15.8093 13.7415 15.2063 9.87651 15.2063Z"
-                                                      stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                                                      stroke-linejoin="round"></path>
-                                                  <path fill-rule="evenodd" clip-rule="evenodd"
-                                                      d="M9.8766 11.886C12.3996 11.886 14.4446 9.841 14.4446 7.318C14.4446 4.795 12.3996 2.75 9.8766 2.75C7.3546 2.75 5.3096 4.795 5.3096 7.318C5.3006 9.832 7.3306 11.877 9.8456 11.886H9.8766Z"
-                                                      stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                                                      stroke-linejoin="round"></path>
-                                                  <path d="M19.2036 8.66919V12.6792" stroke="currentColor" stroke-width="1.5"
-                                                      stroke-linecap="round" stroke-linejoin="round"></path>
-                                                  <path d="M21.2497 10.6741H17.1597" stroke="currentColor" stroke-width="1.5"
-                                                      stroke-linecap="round" stroke-linejoin="round"></path>
-                                              </svg>
-                                          </span>
-                                      </div> -->
-                                      <span class="navbar-toggler-btn">
-                                  <span class="navbar-toggler-icon"></span>
-                              </span>
+                                      <img src="assets/identidade/MenuIcon.png" width="30" alt="">
+                                      
                                   </a>
                                   <ul class="dropdown-menu dropdown-menu-end dropdown-user border-0 p-0 m-0"
                                       aria-labelledby="navbarDropdown">
