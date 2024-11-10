89ab8585d912078b641e076a4fc0337fba402489
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 20:32:36 -0300

    s

--- footer.php
+++ footer.php
@@ -3,17 +3,17 @@
 
         <div class="footer-bottom ">
             <div class="row align-items-center">
-                <div class="logo-hotstar">
+                <!-- <div class="logo-hotstar">
                     <a class="navbar-brand text-primary text-center" href="/">
                         <img class="img-fluid logo mx-auto d-block" src="./assets/identidade/RodapeLogo.png" loading="lazy" alt="streamit" />
                     </a>
-                </div>
+                </div> -->
                 <div class="col-md-6 mt-3" style="color: #7E7E80;">
                     <ul class="menu list-inline p-0 d-flex flex-wrap align-items-center SFProDisplayBold">
                         <li class="menu-item">
                             <a href="#"style="color: #7E7E80;"> Termos de uso </a>
                         </li>
-                        <li id="menu-item-7316" class="menu-item">
+                        <!-- <li id="menu-item-7316" class="menu-item">
                             <a href="#"style="color: #7E7E80;"> Política de Privacidade </a>
                         </li>
                         <li class="menu-item">
@@ -21,7 +21,7 @@
                         </li>
                         <li class="menu-item">
                             <a href="#"style="color: #7E7E80;"> Lista de observação </a>
-                        </li>
+                        </li> -->
                     </ul>
                     <p class="font-size-14">© <span class="currentYear"></span> <span class="text-primary">ROLETA MÁGICA</span>. Todos os direitos reservados. Todos os vídeos informativos neste aplicativo são marcas registradas e todas as imagens e conteúdos relacionados são propriedade do Mágico do Século. <br> <br>A duplicação e plágio disto são estritamente proibidos. </p>
                 </div>
