7eff6f2bf69ecbca1724d1e25e9c8e2f699c0940
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 10:23:18 -0300

    s

--- movie-detail.php
+++ movie-detail.php
@@ -212,6 +212,7 @@ spl_autoload_register("my_autoload"); // Carrega as classes
                                                                     </li>
                                                                 </ul>
                                                                 <div class="d-flex align-items-center gap-1 font-size-12">
+                                                                    <span class="text-body fw-semibold text-capitalize">Aula <?php echo $r['video_indice_aula'] ?></span>
                                                                     <button type="button" class="btn d-block ms-auto me-2 btn-lg mb-1 btn-play rounded-circle text-uppercase position-relative mx-0">
 
                                                                         <i class="fa-solid fa-play"></i>
