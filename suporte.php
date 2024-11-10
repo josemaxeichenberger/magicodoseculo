46d3c6f7a716dfe07ca49cd1756c2c8179f595a1
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 15:21:23 -0300

    s

--- suporte.php
+++ suporte.php
@@ -184,6 +184,26 @@ spl_autoload_register("my_autoload"); // Carrega as classes
     <!-- Streamit Script -->
     <script src="./assets/js/streamit.js" defer></script>
     <script src="./assets/js/swiper.js" defer></script>
+    <script>
+        (() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+    </script>
 </body>
 
 </html>
