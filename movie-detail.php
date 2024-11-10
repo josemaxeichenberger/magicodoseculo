ae8c520a2798349d9ae130fcd57e46916227a601
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 09:40:12 -0300

    s

--- movie-detail.php
+++ movie-detail.php
@@ -149,20 +149,7 @@ spl_autoload_register("my_autoload"); // Carrega as classes
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-5 col-md-12 trailor-video iq-slider d-none d-lg-block">
-                                        <a href="./assets/images/video/trailer.mp4" class="video-open playbtn text-decoration-none"
-                                            tabindex="0">
-                                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
-                                                y="0px" width="80px" height="80px" viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7"
-                                                xml:space="preserve">
-                                                <polygon class="triangle" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"
-                                                    stroke-miterlimit="10" points="73.5,62.5 148.5,105.8 73.5,149.1 "></polygon>
-                                                <circle class="circle" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"
-                                                    stroke-miterlimit="10" cx="106.8" cy="106.8" r="103.3"></circle>
-                                            </svg>
-                                            <span class="w-trailor text-uppercase"> Watch Trailer </span>
-                                        </a>
-                                    </div>
+                                   
                                 </div>
                             </div>
                         </div>
