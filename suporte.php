d84ff4f1e4bd978a9dafed83d1386db5b907e0be
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 15:20:59 -0300

    sa

--- suporte.php
+++ suporte.php
@@ -5,7 +5,7 @@
 
 function my_autoload($pClassName)
 {
-  include('./Class' . "/" . $pClassName . ".class.php");
+    include('./Class' . "/" . $pClassName . ".class.php");
 }
 
 spl_autoload_register("my_autoload"); // Carrega as classes 
@@ -16,121 +16,174 @@ spl_autoload_register("my_autoload"); // Carrega as classes
 <html lang="en" data-bs-theme="dark">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Mágico do Século</title>
-  <!-- Google Font Api KEY-->
-  <meta name="google_font_api" content="AIzaSyBG58yNdAjc20_8jAvLNSVi9E4Xhwjau_k">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Mágico do Século</title>
+    <!-- Google Font Api KEY-->
+    <meta name="google_font_api" content="AIzaSyBG58yNdAjc20_8jAvLNSVi9E4Xhwjau_k">
 
-  <!-- Favicon -->
-  <link rel="shortcut icon" href="./assets/identidade/002.png" />
-  <!-- Library / Plugin Css Build -->
-  <link rel="stylesheet" href="./assets/css/core/libs.min.css" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./assets/identidade/002.png" />
+    <!-- Library / Plugin Css Build -->
+    <link rel="stylesheet" href="./assets/css/core/libs.min.css" />
 
-  <!-- font-awesome css -->
-  <link rel="stylesheet" href="./assets/vendor/font-awesome/css/all.min.css" />
+    <!-- font-awesome css -->
+    <link rel="stylesheet" href="./assets/vendor/font-awesome/css/all.min.css" />
 
-  <!-- Iconly css -->
-  <link rel="stylesheet" href="./assets/vendor/iconly/css/style.css" />
+    <!-- Iconly css -->
+    <link rel="stylesheet" href="./assets/vendor/iconly/css/style.css" />
 
-  <!-- Animate css -->
-  <link rel="stylesheet" href="./assets/vendor/animate.min.css" />
+    <!-- Animate css -->
+    <link rel="stylesheet" href="./assets/vendor/animate.min.css" />
 
-  <!-- SwiperSlider css -->
-  <link rel="stylesheet" href="./assets/vendor/swiperSlider/swiper.min.css">
-
-
+    <!-- SwiperSlider css -->
+    <link rel="stylesheet" href="./assets/vendor/swiperSlider/swiper.min.css">
 
 
 
-  <!-- Streamit Design System Css -->
-  <link rel="stylesheet" href="./assets/css/streamit.min.css?v=5.2.1" />
 
-  <!-- Custom Css -->
-  <link rel="stylesheet" href="./assets/css/custom.min.css?v=5.2.1" />
 
-  <!-- Rtl Css -->
-  <link rel="stylesheet" href="./assets/css/rtl.min.css?v=5.2.1" />
+    <!-- Streamit Design System Css -->
+    <link rel="stylesheet" href="./assets/css/streamit.min.css?v=5.2.1" />
 
-  <!-- Google Font -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300&display=swap" rel="stylesheet">
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="./assets/css/custom.min.css?v=5.2.1" />
+
+    <!-- Rtl Css -->
+    <link rel="stylesheet" href="./assets/css/rtl.min.css?v=5.2.1" />
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300&display=swap" rel="stylesheet">
 
 </head>
 
 <body class="  ">
-  <span class="screen-darken"></span>
-  <!-- loader Start -->
-  <!-- loader Start -->
-  <div class="loader simple-loader">
-    <div class="loader-body">
-      <img src="./assets/images/loader.gif" alt="loader" class="img-fluid " width="300">
+    <span class="screen-darken"></span>
+    <!-- loader Start -->
+    <!-- loader Start -->
+    <div class="loader simple-loader">
+        <div class="loader-body">
+            <img src="./assets/images/loader.gif" alt="loader" class="img-fluid " width="300">
+        </div>
+    </div>
+    <!-- loader END --> <!-- loader END -->
+    <main class="main-content">
+        <?php
+        include(__DIR__ . '/header.php');
+        ?>
+
+        <section class="bg-body">
+            <div class="container py3">
+                <h1>Está com <br>alguma dúvida?</h1>
+                <form class="row g-3 needs-validation" novalidate>
+  <div class="col-md-4">
+    <label for="validationCustom01" class="form-label">First name</label>
+    <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
+    <div class="valid-feedback">
+      Looks good!
     </div>
   </div>
-  <!-- loader END --> <!-- loader END -->
-  <main class="main-content">
-    <?php
-    include(__DIR__ . '/header.php');
-    ?>
-
-<div class="iq-breadcrumb" style="background-image: url(./assets/images/pages/01.webp);">
-         <div class="container-fluid">
-            <div class="row align-items-center">
-                  <div class="col-sm-12">
-                      <nav aria-label="breadcrumb" class="text-center">
-                          <h2 class="title">Pricing Plan</h2>
-                          <ol class="breadcrumb justify-content-center">
-                              <li class="breadcrumb-item"><a href="./index.html">Home</a></li> 
-                              <li class="breadcrumb-item active">Pricing Plan</li>
-                          </ol>
-                      </nav>
-                  </div>
-              </div> 
-         </div>
-      </div>      <!--bread-crumb-->
-
-
-
-
-
-
-
-  </main>
-  <?php include('./footer.php') ?>
-
-
-  <div id="back-to-top" style="display: none;">
-    <a class="p-0 btn bg-primary btn-sm position-fixed top border-0 rounded-circle text-white" id="top" href="#top">
-      <i class="fa-solid fa-chevron-up"></i>
-    </a>
+  <div class="col-md-4">
+    <label for="validationCustom02" class="form-label">Last name</label>
+    <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
+    <div class="valid-feedback">
+      Looks good!
+    </div>
   </div>
-  <!-- Wrapper End-->
-  <!-- Library Bundle Script -->
-  <script src="./assets/js/core/libs.min.js"></script>
-  <!-- Plugin Scripts -->
+  <div class="col-md-4">
+    <label for="validationCustomUsername" class="form-label">Username</label>
+    <div class="input-group has-validation">
+      <span class="input-group-text" id="inputGroupPrepend">@</span>
+      <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+      <div class="invalid-feedback">
+        Please choose a username.
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <label for="validationCustom03" class="form-label">City</label>
+    <input type="text" class="form-control" id="validationCustom03" required>
+    <div class="invalid-feedback">
+      Please provide a valid city.
+    </div>
+  </div>
+  <div class="col-md-3">
+    <label for="validationCustom04" class="form-label">State</label>
+    <select class="form-select" id="validationCustom04" required>
+      <option selected disabled value="">Choose...</option>
+      <option>...</option>
+    </select>
+    <div class="invalid-feedback">
+      Please select a valid state.
+    </div>
+  </div>
+  <div class="col-md-3">
+    <label for="validationCustom05" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="validationCustom05" required>
+    <div class="invalid-feedback">
+      Please provide a valid zip.
+    </div>
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+      <label class="form-check-label" for="invalidCheck">
+        Agree to terms and conditions
+      </label>
+      <div class="invalid-feedback">
+        You must agree before submitting.
+      </div>
+    </div>
+  </div>
+  <div class="col-12">
+    <button class="btn btn-primary" type="submit">Submit form</button>
+  </div>
+</form>
+            </div>
+        </section>
 
 
-  <!-- SwiperSlider Script -->
-  <script src="./assets/vendor/swiperSlider/swiper.min.js"></script>
 
 
 
 
-  <!-- Lodash Utility -->
-  <script src="./assets/vendor/lodash/lodash.min.js"></script>
-  <!-- External Library Bundle Script -->
-  <script src="./assets/js/core/external.min.js"></script>
-  <!-- countdown Script -->
-  <script src="./assets/js/plugins/countdown.js"></script>
-  <!-- utility Script -->
-  <script src="./assets/js/utility.js"></script>
-  <!-- Setting Script -->
-  <script src="./assets/js/setting.js"></script>
-  <script src="./assets/js/setting-init.js" defer></script>
-  <!-- Streamit Script -->
-  <script src="./assets/js/streamit.js" defer></script>
-  <script src="./assets/js/swiper.js" defer></script>
+    </main>
+    <?php include('./footer.php') ?>
+
+
+    <div id="back-to-top" style="display: none;">
+        <a class="p-0 btn bg-primary btn-sm position-fixed top border-0 rounded-circle text-white" id="top" href="#top">
+            <i class="fa-solid fa-chevron-up"></i>
+        </a>
+    </div>
+    <!-- Wrapper End-->
+    <!-- Library Bundle Script -->
+    <script src="./assets/js/core/libs.min.js"></script>
+    <!-- Plugin Scripts -->
+
+
+    <!-- SwiperSlider Script -->
+    <script src="./assets/vendor/swiperSlider/swiper.min.js"></script>
+
+
+
+
+    <!-- Lodash Utility -->
+    <script src="./assets/vendor/lodash/lodash.min.js"></script>
+    <!-- External Library Bundle Script -->
+    <script src="./assets/js/core/external.min.js"></script>
+    <!-- countdown Script -->
+    <script src="./assets/js/plugins/countdown.js"></script>
+    <!-- utility Script -->
+    <script src="./assets/js/utility.js"></script>
+    <!-- Setting Script -->
+    <script src="./assets/js/setting.js"></script>
+    <script src="./assets/js/setting-init.js" defer></script>
+    <!-- Streamit Script -->
+    <script src="./assets/js/streamit.js" defer></script>
+    <script src="./assets/js/swiper.js" defer></script>
 </body>
 
 </html>
