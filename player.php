6484de44ec870e245ec9f7a85f5388addd75b24d
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 10:42:18 -0300

    sa

--- player.php
+++ player.php
@@ -89,7 +89,300 @@ spl_autoload_register("my_autoload"); // Carrega as classes
     </div>
   </div>
 </div>
-
+<div class="details-part">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <!-- Movie Description Start-->
+        <div class="trending-info mt-4 pt-0 pb-4">
+          <div class="row">
+            <div class="col-md-9 col-12 mb-auto">
+              <div class="d-block d-lg-flex align-items-center">
+                <h2 class="trending-text fw-bold texture-text text-uppercase my-0 fadeInLeft animated d-inline-block"
+                  data-animation-in="fadeInLeft" data-delay-in="0.6" style="opacity: 1; animation-delay: 0.6s">
+                  Zombie Island
+                </h2>
+                <div class="slider-ratting d-flex align-items-center ms-lg-3 ms-0">
+                  <ul class="ratting-start p-0 m-0 list-inline text-warning d-flex align-items-center justify-content-left">
+                    <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                    <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                    <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                    <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                    <li><i class="fa fa-star-half" aria-hidden="true"></i></li>
+                  </ul>
+                  <span class="text-white ms-2">4.8 (imdb)</span>
+                </div>
+              </div>
+              <ul class="p-0 mt-2 list-inline d-flex flex-wrap movie-tag">
+                <li class="trending-list"><a class="text-primary" href="./view-all-movie.html">Action</a></li>
+                <li class="trending-list"><a class="text-primary" href="./view-all-movie.html">Adventure</a></li>
+                <li class="trending-list"><a class="text-primary" href="./view-all-movie.html">Drama</a></li>
+              </ul>
+              <div class="d-flex flex-wrap align-items-center text-white text-detail flex-wrap mb-4">
+                <span class="badge bg-secondary">Horror</span>
+                <span class="ms-3 font-Weight-500 genres-info">1hr 48mins</span>
+                <span class="trending-year trending-year-list font-Weight-500 genres-info">
+                  Feb 2017
+                </span>
+              </div>
+              <div class="d-flex align-items-center gap-4 flex-wrap mb-4">
+                <ul class="list-inline p-0 share-icons music-play-lists mb-n2 mx-n2">
+                  <li class="share">
+                    <span><i class="fa-solid fa-share-nodes"></i></span>
+                    <div class="share-box">
+                      <svg width="15" height="40" viewBox="0 0 15 40" class="share-shape" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                          d="M14.8842 40C6.82983 37.2868 1 29.3582 1 20C1 10.6418 6.82983 2.71323 14.8842 0H0V40H14.8842Z"
+                          fill="#191919"></path>
+                      </svg>
+                      <div class="d-flex align-items-center">
+                        <a href="#" class="share-ico"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#" class="share-ico"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#" class="share-ico"><i class="fa-solid fa-link"></i></a>
+                      </div>
+                    </div>
+                  </li>
+                  <li><span><i class="fa-solid fa-heart"></i></span></li>
+                  <li><span><i class="fa-solid fa-plus"></i></span></li>
+                  <li><span><a href="./assets/images/video/trailer.mp4" download><i class="fa-solid fa-download"></i></a></span></li>
+                </ul>
+                <div class="movie-detail-select">
+                  <select name="movieselect" class="form-control movie-select select2-basic-single js-states">
+                    <option value="1">Playlist</option>
+                    <option value="2">Zombie Island</option>
+                    <option value="3">Sand Dust</option>
+                    <option value="4">Jumbo Queen</option>
+                  </select>
+                </div>
+              </div>
+              <ul class="iq-blogtag list-unstyled d-flex flex-wrap align-items-center gap-3 p-0">
+                <li class="iq-tag-title text-primary mb-0">
+                  <i class="fa fa-tags" aria-hidden="true"></i>
+                  Tags:
+                </li>
+                <li><a class="title" href="./view-all-movie.html">Action</a><span
+                    class="text-secondary">,</span></li>
+                <li><a class="title" href="./view-all-movie.html">Adventure</a><span
+                    class="text-secondary">,</span></li>
+                <li><a class="title" href="./view-all-movie.html">Drama</a></li>
+              </ul>
+            </div>
+            <div class="trailor-video col-md-3 col-12 mt-lg-0 mt-4 mb-md-0 mb-1 text-lg-right">
+              <a data-fslightbox="html5-video" href="https://www.youtube.com/watch?v=QCGq1epI9pQ"
+                class="video-open playbtn block-images position-relative playbtn_thumbnail">
+                <img src="./assets/images/genre/01.webp"
+                  class="attachment-medium-large size-medium-large wp-post-image" alt="" loading="lazy" />
+                <span class="content btn btn-transparant iq-button">
+                  <i class="fa fa-play me-2 text-white"></i>
+                  <span>Trailer Link</span>
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- Movie Description End -->        <!-- Movie Source Start -->
+        <div class="content-details trending-info">
+          <ul class="iq-custom-tab tab-bg-gredient-center d-flex nav nav-pills align-items-center text-center mb-5 justify-content-center list-inline"
+            role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active show" data-bs-toggle="pill" href="#description-01" role="tab" aria-selected="true">
+                Description
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="pill" href="#review-01" role="tab" aria-selected="false">
+                Rate &amp; Review
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="pill" href="#source-01" role="tab" aria-selected="false">
+                Sources
+              </a>
+            </li>
+          </ul>
+          <div class="tab-content">
+            <div id="description-01" class="tab-pane animated fadeInUp active show" role="tabpanel">
+              <div class="description-content">
+                <p>
+                  Zombie Island is a 1998 direct-to-video animated comedy horror film
+                  based on Hanna-Barbera's Scooby-Doo Saturday-morning cartoons. In the
+                  film, Shaggy, Scooby, Fred, Velma, and Daphne reunite after a
+                  year-long hiatus from Mystery, Inc. to investigate a bayou island said
+                  to be haunted by the ghost of the pirate Morgan Moonscar. The film was
+                  directed by Jim Stenstrum, from a screenplay by Glenn Leopold.
+                </p>
+              </div>
+            </div>
+            <div id="review-01" class="tab-pane animated fadeInUp" role="tabpanel">
+              <div class="streamit-reviews">
+                <div id="comments" class="comments-area validate-form">
+                  <p class="masvideos-noreviews mt-3">
+                    There are no reviews yet.
+                  </p>
+                </div>
+                <div class="review_form">
+                  <div class="comment-respond">
+                    <h3 class="fw-500 my-2">
+                      Be the first to review “Zombie Island”
+                    </h3>
+                    <div class="row">
+                      <div class="col-md-12">
+                          <div class="form-group">
+                              <label class="mb-2">
+                                  Your review
+                                  <span class="required">
+                                  *
+                                  </span>
+                              </label>
+                              <textarea class="form-control" name="comment" cols="5" rows="8" required=""></textarea>
+                          </div>
+                      </div>
+                      <div class="col-md-6">
+                          <div class="form-group">
+                              <label class="mb-2">
+                                  Name
+                                  <span class="required">
+                                  *
+                                  </span>
+                              </label>
+                              <input class="form-control" name="author" type="text" value="" size="30" required="" />
+                          </div>
+                      </div>
+                      <div class="col-md-6">
+                          <div class="form-group">
+                              <label class="mb-2">
+                                  Email&nbsp;
+                                  <span class="required">
+                                  *
+                                  </span>
+                              </label>
+                              <input class="form-control" name="email" type="email" value="" size="30" required="" />
+                          </div>
+                      </div>
+                      <div class="col-md-12">
+                          <div class="mt-3 mt-3 d-flex gap-2 align-items-center">
+                              <input class="form-check-input mt-0" type="checkbox" value="" id="check1" checked />
+                              <label class="form-check-label" for="check1">
+                                  Save my name, email, and website in this browser for the
+                                  next time I comment.
+                              </label>
+                          </div>
+                      </div>
+                      <div class="col-md-12">
+                          <div class="form-submit mt-4">
+                              <div class="iq-button">
+                                  <button name="submit" type="submit" id="submit" class="btn text-uppercase position-relative" value="Submit">
+                                      <span class="button-text">Submit</span>
+                                      <i class="fa-solid fa-play"></i>
+                                  </button>
+                              </div>
+                          </div>
+                      </div>
+                  </div>  
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id="source-01" class="tab-pane animated fadeInUp" role="tabpanel">
+              <div class="source-list-content table-responsive">
+                <table class="table custom-table">
+                  <thead>
+                    <tr>
+                      <th>
+                        Links
+                      </th>
+                      <th>
+                        Quality
+                      </th>
+                      <th>
+                        Language
+                      </th>
+                      <th>
+                        Player
+                      </th>
+                      <th>
+                        Date Added
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div class="iq-button">
+                          <a href="./movie-detail.html" class="btn text-uppercase position-relative">
+                            <span class="button-text"> Play Now</span>
+                            <i class="fa-solid fa-play"></i>                 
+                          </a>
+                        </div>
+                      </td>
+                      <td>
+                        1080p
+                      </td>
+                      <td>
+                        english
+                      </td>
+                      <td>
+                        MusicBee
+                      </td>
+                      <td>
+                        2021-11-28
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div class="iq-button">
+                          <a href="./movie-detail.html" class="btn text-uppercase position-relative">
+                            <span class="button-text"> Play Now</span>
+                            <i class="fa-solid fa-play"></i>                 
+                          </a>
+                        </div>
+                      </td>
+                      <td>
+                        800p
+                      </td>
+                      <td>
+                        english
+                      </td>
+                      <td>
+                        5KPlayer
+                      </td>
+                      <td>
+                        2021-11-25
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div class="iq-button">
+                          <a href="./movie-detail.html" class="btn text-uppercase position-relative">
+                            <span class="button-text"> Play Now</span>
+                            <i class="fa-solid fa-play"></i>                 
+                          </a>
+                        </div>
+                      </td>
+                      <td>
+                        720p
+                      </td>
+                      <td>
+                        English
+                      </td>
+                      <td>
+                        MediaMonkey
+                      </td>
+                      <td>
+                        2021-11-20
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Movie Source End -->      </div>
+    </div>
+  </div>
+</div>
 
 
         <section>
