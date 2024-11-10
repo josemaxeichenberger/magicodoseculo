a15dea8ca4385fa9d897050bb3cf99503eea1779
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 16:33:52 -0300

    s

--- suporte.php
+++ suporte.php
@@ -122,7 +122,7 @@ spl_autoload_register("my_autoload"); // Carrega as classes
         <section class="contact bg-light container-fluid">
             <div class="container bg-light">
                 <h1 class=" text-dark">Suporte <br><span>Especiaalizado</span></h1>
-                <p>
+                <p class=" text-dark">
                     Nesta aba você encontra todos os meios de comunicação para ter um suporte especializado, você poderá tirar suas dúvidas e possíveis problemas no decorrer da sua jornada, nossa equipe está pronta ara te ajudar em qualquer questão!
                 </p>
                 <div class="input-group input-group-lg">
