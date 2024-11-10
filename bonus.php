b6fac76b8440cd7ead78749da17bb6d1c2ec3f30
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 19:25:05 -0300

    sa

--- bonus.php
+++ bonus.php
@@ -100,8 +100,8 @@ spl_autoload_register("my_autoload"); // Carrega as classes
         ?>
 
         <div class="section-padding">
-            <div class="container">
-                <h1 class="text-white">Seus Bônus</h1>
+            <div class="container pt-5">
+                <h1 class="text-white pt-5">Seus Bônus</h1>
                 <div class="row row-cols-xl-3 row-cols-md-2 row-cols-1">
                     <div class="col px-5 px-xl-1">
                         <div class="iq-blog-box">
