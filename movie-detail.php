8a8e24046dbde7f87b80361e1db84723be13cbd2
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 10:18:53 -0300

    s

--- movie-detail.php
+++ movie-detail.php
@@ -192,7 +192,7 @@ spl_autoload_register("my_autoload"); // Carrega as classes
                                                         <img src="<?php echo $r['video_capa'] ?>" alt="movie-card" class="img-fluid rounded-4  w-100 d-block border-0">
                                                         <div class="position-relative px-2" style="top: -6rem;">
                                                             <div class=" align-items-center">
-                                                                <h5 class="text-capitalize fw-500 position-relative" style=" top:1.2rem;"> <a href="">Play List 1</a> </h5>
+                                                                <!-- <h5 class="text-capitalize fw-500 position-relative" style=" top:1.2rem;"> <a href="">Play List 1</a> </h5> -->
                                                                 <div class="d-flex align-items-center gap-1 font-size-12">
                                                                     <i class="fa-solid fa-earth-americas text-primary"></i>
                                                                     <span class="text-body fw-semibold text-capitalize">Public</span>
