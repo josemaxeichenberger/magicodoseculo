0d9760809435011ab0325275fe1a4c9027fc1fb7
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 18:50:40 -0300

    s

--- header.php
+++ header.php
@@ -60,7 +60,7 @@
                                       </li>
                                       <li class="py-2 elemento">
                                           <a href="./login.php"
-                                              class=" mt-1 d-flex justify-content-center align-content-center gap-2" style="">
+                                              class=" mt-1 d-flex justify-content-center align-content-center gap-2" style="align-items:center !important;">
                                             
                                               <h5 class="mb-0 font-size-18 SFProDisplayRegular">Sair</h5>
                                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"
