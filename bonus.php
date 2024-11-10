1c8691436ef3f5bd101b8701284085f7bbd9be1e
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-10 20:01:51 -0300

    s

--- bonus.php
+++ bonus.php
@@ -116,7 +116,7 @@ spl_autoload_register("my_autoload"); // Carrega as classes
 
         <div class="section-padding">
             <div class="container pt-5">
-                <h1 class="text-white pt-5 text-center" ><img src="assets/identidade/Grupo.png" width="55" alt=""> Seus Bônus</h1>
+                <h1 class="text-white pt-5 text-center align-bottom" ><img src="assets/identidade/Grupo.png" width="55" alt=""> Seus Bônus</h1>
                 <hr class="divider">
                 <div class="row row-cols-xl-3 pt-3 row-cols-md-2 row-cols-1">
                     <div class="col px-5 px-xl-1">
