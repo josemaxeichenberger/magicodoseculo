42a720e78a047f212fbe0a88fc1694863ed5c0b1
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-11 03:31:49 -0300

    sa

--- player.php
+++ player.php
@@ -12,7 +12,7 @@ function my_autoload($pClassName)
 spl_autoload_register("my_autoload"); // Carrega as classes 
 $link = '';
 
-$uteis = '   <div class="row text-center pt-3 pt-xl-1"> 
+$uteisCasaLicenciada = '   <div class="row text-center pt-3 pt-xl-1"> 
           <div class="col-12 mb-2 mt-2">
             <a class="text-white" target="_blank" rel="noopener noreferrer">
               <svg xmlns="http://www.w3.org/2000/svg" style="width: 25px;" fill="#ffffff" viewBox="0 0 640 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
@@ -23,10 +23,21 @@ $uteis = '   <div class="row text-center pt-3 pt-xl-1">
           </div> 
            <div class="col-12 mt-2">
                 <a href="https://bit.ly/Casa-Licenciada" target="_black" class="btn  mx-1 btn-info btn-sm text-uppercase">Crie sua conta aqui</a>
-                <a href="https://chat.whatsapp.com/Hzg6obaAr0yJhbdYAZRUqN" target="_black" class="btn mx-1 btn-warning btn-sm text-uppercase">GRUPO DE LIVES</a>
            </div>                  
         </div> ';
-
+        $uteisGrupodelives = '   <div class="row text-center pt-3 pt-xl-1"> 
+        <div class="col-12 mb-2 mt-2">
+          <a class="text-white" target="_blank" rel="noopener noreferrer">
+            <svg xmlns="http://www.w3.org/2000/svg" style="width: 25px;" fill="#ffffff" viewBox="0 0 640 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+              <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
+            </svg>
+            LINKS ÚTEIS
+          </a>
+        </div> 
+         <div class="col-12 mt-2">
+              <a href="https://chat.whatsapp.com/Hzg6obaAr0yJhbdYAZRUqN" target="_black" class="btn mx-1 btn-warning btn-sm text-uppercase">GRUPO DE LIVES</a>
+         </div>                  
+      </div> ';
 ?>
 <!doctype html>
 <html lang="en" data-bs-theme="dark">
@@ -144,8 +155,16 @@ $uteis = '   <div class="row text-center pt-3 pt-xl-1">
                                         <span class="ms-3 font-Weight-500 genres-info"><?php echo $res['video_duracao']; ?></span>
 
                                     </div>
-                                    <span class="mx-auto d-block text-center font-Weight-500 genres-info"><?php echo  $link ?> </span>
-                                    <?php echo $uteis; ?>
+                                    
+                                    <?php
+                                        if($res['modulo_id'] ==1 && $res['video_indice_aula'] ==2){
+                                            echo $uteisCasaLicenciada;
+                                        }
+                                        if($res['modulo_id'] ==4 && $res['video_indice_aula'] ==1){
+                                            echo $uteisGrupodelives;
+                                        }
+                                    
+                                     ?>
                                 </div>
 
                             </div>
