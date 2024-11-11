a3898c70bf7f2a02ec10b9fd2b203dcc7e08b837
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-11 10:48:10 -0300

    s

--- login.php
+++ login.php
@@ -96,7 +96,7 @@
                                     <label class="custom-checkbox mb-3 ">
                                         <input type="checkbox" name="termos" id="termos" required="">
                                         <span class="checkmark"></span>
-                                        Aceito os termos e condições
+                                        Ao clicar, você concorda com nossos <a href="#" data-bs-toggle="modal" data-bs-target="#staticBackdrop">&nbsp; termos e condições</a> 
                                     </label>
                                     <small class="form-text text-danger termosError" id="termosError"></small>
                                 </div>
@@ -115,7 +115,64 @@
             </div>
         </div>
     </main>
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Termos </h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <h4>Termos de Uso do Curso de Roleta:
+        </h4>
+        <p>
+        Este documento estabelece os Termos de Uso do Curso de Roleta (“Curso”) oferecido pela RM2 / Mágico do Século . Ao adquirir e participar deste Curso, você concorda em aderir aos termos e condições descritos abaixo, que têm como objetivo proporcionar um entendimento claro sobre a natureza do conteúdo oferecido e suas expectativas.
+        </p>
+        <h3>1. Finalidade do Curso
+        </h3>
+        <p>1.1. O Curso de Roleta é um programa educacional desenvolvido para fornecer conhecimentos teóricos e práticos sobre estratégias, probabilidades e padrões observáveis nas mesas de roleta. 1.2. Este Curso é estritamente destinado a fins educacionais e de entretenimento. Ele não é um substituto para aconselhamento financeiro, consultoria de investimentos ou outros serviços profissionais relacionados a ganhos financeiros. 1.3. A roleta é um jogo de azar. Todos os conteúdos, dicas, estratégias e práticas apresentadas no Curso têm como objetivo apenas o aprimoramento da experiência de jogo do usuário e não devem ser interpretados como garantia de lucro ou resultados financeiros específicos.</p>
+        <h3>2. Isenção de Responsabilidade
+        </h3>
+        <p>
+        2.1. O Curso não oferece qualquer tipo de promessa ou garantia de ganhos financeiros. 2.2. O Curso não incentiva e promove o uso da roleta como uma forma de investimento, e quaisquer resultados financeiros que venham a ser obtidos durante ou após a participação no Curso são de responsabilidade exclusiva do usuário. 2.3. O participante entende que qualquer aposta realizada na roleta é por conta e risco próprios, e que o jogo deve ser encarado como uma forma de entretenimento, podendo resultar em perdas financeiras.
+        </p>
+        <h3>
+        3. Restrição de Idade
 
+        </h3>
+        <p>
+        3.1. O Curso é estritamente destinado a maiores de 18 anos. 3.2. A participação de menores de idade é expressamente proibida e qualquer tentativa de acesso por menores de 18 anos resultará no cancelamento imediato do acesso ao Curso, sem direito a reembolso. 3.3. A RM2 / Mágico do Século  não se responsabiliza pelo acesso ao Curso por parte de menores de idade, cabendo ao usuário a responsabilidade de respeitar essa restrição
+        </p>
+        <h3>4. Jogo Responsável
+        </h3>
+        <p>4.1. O participante é encorajado a jogar com responsabilidade, estabelecendo limites claros de tempo e dinheiro para suas atividades de entretenimento com a roleta. 4.2. É fundamental que o participante utilize apenas recursos financeiros que esteja disposto a perder, evitando apostar valores que possam comprometer sua estabilidade financeira ou bem-estar pessoal. 4.3. O usuário deve procurar ajuda profissional em casos de comportamento compulsivo ou problemas relacionados ao jogo, por meio de canais de suporte especializados.</p>
+        <h3>5. Natureza do Jogo de Roleta
+        </h3>
+        <p>5.1. A roleta é considerada um jogo de azar, sendo impossível prever ou garantir resultados consistentes de ganhos. 5.2. Quaisquer estratégias ensinadas no Curso são baseadas em teorias estatísticas e análises empíricas, mas não devem ser encaradas como métodos infalíveis de lucro. 5.3. O participante deve estar ciente dos riscos envolvidos na prática de jogos de azar, reconhecendo que a roleta não é uma forma de investimento ou fonte confiável de renda.</p>
+        <h3>6. Responsabilidade do Participante
+        </h3>
+        <p>
+        6.1. O participante é responsável por gerenciar de forma prudente os recursos destinados ao jogo, utilizando apenas o que estiver disposto a perder. 6.2. É recomendado que o participante jogue de maneira responsável, estabelecendo limites de tempo e dinheiro para suas atividades de entretenimento. 6.3. A RM2 / Mágico do Século  não se responsabiliza por decisões financeiras ou de apostas feitas pelo participante com base no conteúdo do Curso.
+
+        </p>
+        <h3>
+        7. Termos de Compromisso
+
+        </h3>
+        <p>7.1. Ao se inscrever no Curso, o participante declara que está ciente de que todo o conteúdo é destinado a fins de entretenimento e aprendizado, sem garantia de ganhos financeiros. 7.2. O participante concorda em não buscar indenizações ou compensações financeiras da RM2 / Mágico do Século  por quaisquer perdas decorrentes de suas atividades relacionadas à roleta.</p>
+        <h3>8. Alterações nos Termos
+        </h3>
+        <p>8.1. A RM2 / Mágico do Século  se reserva o direito de modificar estes Termos de Uso a qualquer momento, notificando os participantes por meio de comunicado eletrônico ou na plataforma do Curso. 8.2. O participante é responsável por revisar os Termos de Uso periodicamente para estar ciente de quaisquer atualizações ou alterações.
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
     <?php include_once('./footer.php'); ?>
 
     <div id="back-to-top" style="display: none;">
