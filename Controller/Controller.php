6d7ba21df67c210bf5fcc9b8d4df19d1951df7dd
josemaxeichenberger <jme.jose.max@gmail.com>
2024-11-13 16:50:08 -0300

    s

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -12,6 +12,17 @@ $videos_like->setId_usuario($_POST['id_usuario']);
 $videos_like->setId_video($_POST['id_video']);
 $videos_like->setTipo($_POST['tipo']);
 $res = $videos_like->Insert();
-
-echo  json_encode(['status'=>200 , 'data' => $res]);
+if($res){
+    http_response_code(200);
+    echo  json_encode(['status'=>200 , 'data' => $res]);
 }
+}
+
+if(isset($_GET['action']) == 'getLikes'){
+    $videos_like = new videos_like();    
+    $videos_like->setId_video($_GET['id_video']);
+    $videos_like->setTipo($_GET['tipo']);
+    $res = $videos_like->SelectCount();
+    
+    echo  json_encode(['status'=>200 , 'data' => $res]);
+    }
